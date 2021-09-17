4503c020284ad4598ded13b3724f91d180caa699
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2021-09-17 22:03:44 +0000

    Update with repli

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ require('jstransformer-markdown-it'); // This needs to be installed for Pug to u
 const app = express();
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.send(pug.renderFile('public/index.pug')); // We use pug.renderFile to render a file.
+app.get('/', function(req, res) {
+  res.send(pug.renderFile('public/index.pug')); // We use pug.renderFile to render a file.
 });
 
 // Add more URLs and Pug files here...
 
 app.listen(8080, function() {
-    console.log('Server up!');
+  console.log('Server up!');
 });
