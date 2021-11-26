44ece7985d829314b7e19730bef732720acf183f
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2021-11-25 22:44:32 -0300

    update script for badge

---
+++ docs/script.js
@@ -0,0 +1,11 @@
+{
+    "schemaVersion": 1,
+    "style": "for-the-badge",
+    "label": "the site",
+    "logo": "c",
+    "logoColor": "pink",
+    "message": "is online",
+    "color": "black",
+    "url": "https://dev.camilaloliveira.com.br",
+    "alt": "Badge who says the site is online"
+};
